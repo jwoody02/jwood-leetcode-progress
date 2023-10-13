bfa13b49e84f36adc4ce21767fab178fd63733b7
Jordan Wood <89751870+jwoody02@users.noreply.github.com>
2023-10-12 21:21:16 -0600

    Time: 36ms (93.31%), Space: 16.60MB (24.93%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,22 +1,33 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # so we get O(1) lookup
-        wordSet = set(wordDict)
-        
-        # s = "leetcode", wordDict = ["leet","code"]
-        # Dynamic Programming Solution:
-        # dp = [false, false, false, True, false, false, false, True] => does the end of a word in the dictionary happen here
-        dp = [True] + [False] * len(s)
+        # return val
+        ret_val = False
 
-        # iterate from 0...len(s) + 1 since substring starts at 1
-        for i in range(len(s) + 1):
-            for j in range(i):
-                # valid word
-                if s[j:i] in wordSet:
-                    # make sure we can get to this word
-                    if dp[j]:
-                        dp[i] = True
-                        break
-                    
-        return dp[-1]
+        # so we can O(1) retrieval
+        wordSet = set(wordDict)
+
+        # index cache/memoisation
+        index_cache = {}
+
+        def dfs(index):
+            # make it so we can edit ret_val
+            nonlocal ret_val
+            
+            # check if we've already handled index
+            if index in index_cache:
+                return
+
+            # reached the end
+            if index == len(s):
+                ret_val = True
+                return
+            
+            # call dfs on valid words
+            for i in range(index, len(s) + 1):
+                if s[index:i] in wordSet:
+                    index_cache[index] = 0
+                    dfs(i)
+        
+        dfs(0)
+        return ret_val
 
