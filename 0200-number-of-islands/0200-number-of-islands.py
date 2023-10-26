79838359982e8a72f99d88f51db2793602202cb8
Jordan Wood <89751870+jwoody02@users.noreply.github.com>
2023-10-25 22:48:49 -0600

    Time: 332ms (27.96%), Space: 18.85MB (69.60%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,40 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        num_islands = 0
+
+        # helper func
+        def isLand(m: int, n: int):
+            return isValid(m, n) and grid[m][n] == "1"
+        def setToWater(m: int, n: int):
+            grid[m][n] = "0"
+        def isValid(m: int, n: int):
+            return m < len(grid) and m >= 0 and n < len(grid[m]) and n >= 0
+
+        # dfs that takes array of coordinates as input
+        def dfs(coordinate):
+            if not coordinate:
+                return
+            
+            (x,y) = coordinate
+            if isValid(x,y):
+                setToWater(x,y)
+                if isLand(x + 1, y):
+                    dfs((x + 1, y))
+                if isLand(x, y + 1):
+                    dfs((x, y + 1))
+                if isLand(x - 1, y):
+                    dfs((x - 1, y))
+                if isLand(x, y - 1):
+                    dfs((x, y - 1))
+
+        for m in range(len(grid)):
+            for n in range(len(grid[m])):
+                # call dfs if land
+                if isLand(m,n):
+                    num_islands += 1
+
+                    # the goal of dfs here is to find the connected land
+                    # and set it to water(0) so we can keep track of the islands
+                    dfs((m,n)) 
+
+        return num_islands
