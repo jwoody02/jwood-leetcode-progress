47aebffd71f6e2c1924bb3a21087c946146d4443
Jordan Wood <89751870+jwoody02@users.noreply.github.com>
2023-10-25 22:23:42 -0600

    Time: 588ms (72.06%), Space: 30.52MB (24.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        #  Kadane's algorithm
         for i in range(1, len(nums)):
             if nums[i-1] > 0:
                 nums[i] += nums[i-1]
