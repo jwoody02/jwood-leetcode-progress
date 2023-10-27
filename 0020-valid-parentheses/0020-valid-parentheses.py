969e70dd1802e32a188493027604eef29112e7a2
Jordan Wood <89751870+jwoody02@users.noreply.github.com>
2023-10-26 18:12:59 -0600

    Time: 42ms (31.82%), Space: 16.38MB (27.26%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # queue to keep track of the order of open parentheses 
+        queue = []
+
+        # helper func to return closing bracket given character
+        def closingBracket(s:str) -> str:
+            # not valid
+            if len(s) < 1:
+                return ""
+            
+            # return the proper closing bracket
+            character = s[0]
+
+            if character == "(": return ")"
+            elif character == "[": return "]"
+            elif character == "{": return "}"
+            else: return ""
+        
+        # helper func to return true/false if an open bracket
+        def isOpenBracket(s:str) -> bool:
+            return len(s) == 1 and (s[0] == "[" or s[0] == "(" or s[0] == "{")
+        
+        # return value
+        ret_value = True
+
+        # iterate thru input string
+        for c in s:
+            if isOpenBracket(c):
+                queue.append(closingBracket(c))
+            elif len(queue) > 0:
+                # make sure it's the proper bracket
+                expected_character = queue.pop()
+                if c != expected_character:
+                    ret_value = False
+            else:
+                ret_value = False
+        if len(queue) != 0: ret_value = False
+
+        return ret_value
