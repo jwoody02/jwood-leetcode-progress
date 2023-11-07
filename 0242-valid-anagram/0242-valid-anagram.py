19a1b50202433a2af207b8d9a9c58aef307a1cc1
Jordan Wood <89751870+jwoody02@users.noreply.github.com>
2023-11-06 17:21:14 -0700

    Time: 67ms (8.22%), Space: 16.80MB (66.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) < len(t): s, t = t, s
+        if len(s) != len(t): return False
         s_map = {}
         for char in s:
             s_map.update({char: s_map.get(char, 0) + 1})
@@ -9,9 +9,4 @@ class Solution:
         for char in t:
             t_map.update({char: t_map.get(char, 0) + 1})
         
-        for char in s:
-            if char not in t_map:
-                return False
-            if s_map[char] != t_map[char]:
-                return False
-        return True
+        return s_map == t_map
