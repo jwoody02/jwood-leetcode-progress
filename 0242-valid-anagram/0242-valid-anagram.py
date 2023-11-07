e055bfa512dd7db2ff405d8758d3016d3feeabda
Jordan Wood <89751870+jwoody02@users.noreply.github.com>
2023-11-06 17:24:15 -0700

    Time: 82ms (5.21%), Space: 16.71MB (66.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t): return False
-        s_map = {}
+        dic = {}
         for char in s:
-            s_map.update({char: s_map.get(char, 0) + 1})
+            dic.update({char: dic.get(char, 0) + 1})
         
-        t_map = {}
         for char in t:
-            t_map.update({char: t_map.get(char, 0) + 1})
+            if char not in dic: return False
+            dic.update({char: dic.get(char, 0) - 1})
         
-        return s_map == t_map
+        for val in dic.values():
+            if val != 0: return False
+
+        return True
